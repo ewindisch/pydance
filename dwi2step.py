b24630e1d2e897c006780af581e244aebbd6613e
tgz <tgz@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2003-02-19 22:41:04 +0000

    Patched for better sync by robepisc, and an eof patch from leo, onto testing

--- dwi2step.py
+++ dwi2step.py
@@ -7,6 +7,8 @@ import os, pygame, pygame.font, pygame.image, pygame.mixer, pygame.movie, time, 
 from pygame.locals import *
 from Numeric import *
 
+VERSION = "0.51"
+
 # so it's currently in one routine. shut up, I'm learning python =]
 def main():
   print "dwi2step, by theGREENzebra (tgz@clickass.org)"
@@ -29,16 +31,13 @@ def main():
   # that instead
   bpm = ''
   gap = ''
-  skipnum = ''
   stepmodes = ['sixty','twtfr','steps','eight','qurtr','halfn','whole']
   stepbeats = [ 1/64.0, 1/24.0,     1 ,     2 ,     4 ,     8 ,    16 ]
   rar = 0
-  i = -1
   bpms = []
-  beats = []
-  bpmstring = ''
-  gapstring = ''
-  beatstring = ''
+  delay_times = []
+  bpmchange_beats = []
+  delay_beats = []
 
 #  arrstat = zeros((4,1024,3))
   print sys.argv[0]
@@ -56,7 +55,7 @@ def main():
     stepfilename = stepfilename[:-4]+".step"
     print 'Opening STEP file:',stepfilename,'..'
     stepfile = open(stepfilename,"w") #but for writing
-    stepfile.write("version 0.5\n")
+    stepfile.write("version " + VERSION + "\n")
     print "debugging crap follows"
     print "----------------------"
     print "wrote header"
@@ -77,29 +76,54 @@ def main():
         print "found songname: ", songname
         stepfile.writelines("song "+songname+"\n")
       if fileline[0:5] == '#BPM:':
-        bpm = float(fileline[5:(len(fileline)-1)])-0.05
+        bpm = float(fileline[5:(len(fileline)-1)])
         print "found bpm: ", bpm
+        stepfile.writelines("bpm "+"%.2f"%bpm+"\n")
       if fileline[0:11] == '#CHANGEBPM:':
-        bpmchanges = 1
         parsemode = 1
+        beat_string = ''
+        bpmchange_string = ''
         print "found bpm changes: "
         for i in fileline[11:-1]:
           if i == '=':   
-            beats.append(float(beatstring))
+            bpmchange_beats.append(float(beat_string))
             parsemode = 2
-            bpmstring = ''
+            bpmchange_string = ''
           elif i == ',':
-            bpms.append(float(bpmstring))
+            bpms.append(float(bpmchange_string))
             parsemode = 1
-            beatstring = ''
+            beat_string = ''
           else:
             if parsemode==1:
-              beatstring += i
+              beat_string += i
             if parsemode==2:
-              bpmstring += i
-        bpms.append(float(bpmstring))
-        for i in beats:
-          print "  at beat",i,"change bpm to",bpms[beats.index(i)]
+              bpmchange_string += i
+        bpms.append(float(bpmchange_string))
+        for i in bpmchange_beats:
+          print "  at beat",i,"change bpm to",bpms[bpmchange_beats.index(i)]
+        print
+      if fileline[0:8] == '#FREEZE:':
+        parsemode = 1
+        beat_string = ''
+        delay_string = ''
+        print "frozen beats: "
+        for i in fileline[8:-1]:
+          if i == '=':
+            delay_beats.append(float(beat_string))
+            parsemode = 2
+            delay_string = ''
+          elif i == ',':
+            delay_times.append(float(delay_string))
+            parsemode = 1
+            beat_string = ''
+          else:
+            if parsemode==1:
+              beat_string += i
+            if parsemode==2:
+              delay_string += i
+        delay_times.append(float(delay_string))
+        for i in delay_beats:
+          print "  at beat",i,"freeze",delay_times[delay_beats.index(i)]
         print
       if fileline[0:8] == '#ARTIST:':
         groupname = fileline[8:(len(fileline)-1)]
@@ -107,7 +131,8 @@ def main():
         stepfile.writelines("group "+groupname+"\n")
       if fileline[0:5] == '#GAP:':
         gap = int(fileline[5:-1])
-        print "found gap: ", gap
+        print "found gap: ", gap, "( => offset = ", -gap, ")"
+        stepfile.writelines("offset "+repr(-gap)+"\n")
       if fileline[0:14] == '#SINGLE:BASIC:':
         onbeat = 0
         diff = int(fileline[14])
@@ -116,14 +141,6 @@ def main():
         fileline = fileline[16:] + "\n"
         dc = ''
         stepmode = "eight"
-        if skipnum < 0:
-          print "padding eighths.."
-          for i in range(abs(skipnum)):
-            stepfile.writelines("eight 00 00 00 00\n")
-            onbeat += stepbeats[stepmodes.index(stepmode)]
-          i = 0
-        else:
-          i = skipnum
         rar = 1
       if fileline[0:14] == '#SINGLE:TRICK:':
         onbeat = 0
@@ -133,14 +150,6 @@ def main():
         fileline = fileline[16:] + "\n"
         dc = ''
         stepmode = "eight"
-        if skipnum < 0:
-          print "padding eighths.."
-          for i in range(abs(skipnum)):
-            stepfile.writelines("eight 00 00 00 00\n")
-            onbeat += stepbeats[stepmodes.index(stepmode)]
-          i = 0
-        else:
-          i = skipnum
         rar = 1
       if fileline[0:15] == '#SINGLE:MANIAC:':
         onbeat = 0
@@ -150,14 +159,6 @@ def main():
         fileline = fileline[17:] + "\n"
         dc = ''
         stepmode = "eight"
-        if skipnum < 0:
-          print "padding eighths.."
-          for i in range(abs(skipnum)):
-            stepfile.writelines("eight 00 00 00 00\n")
-            onbeat += stepbeats[stepmodes.index(stepmode)]
-          i = 0
-        else:
-          i = skipnum
         rar = 1
       if fileline[0:16] == '#SINGLE:ANOTHER:':
         onbeat = 0
@@ -167,29 +168,27 @@ def main():
         fileline = fileline[18:] + "\n"
         dc = ''
         stepmode = "eight"
-        if skipnum < 0:
-          print "padding eighths.."
-          for i in range(abs(skipnum)):
-            stepfile.writelines("eight 00 00 00 00\n")
-            onbeat += stepbeats[stepmodes.index(stepmode)]
-          i = 0
-        else:
-          i = skipnum
         rar = 1
       
+      i = 0
       while rar:
         steps = []
 
-        for xyz in beats:    #check to see if we need to change the bpm
+        for xyz in bpmchange_beats:    #check to see if we need to change the bpm
           if xyz == onbeat:
-            stepfile.writelines("chbpm "+repr(bpms[beats.index(onbeat)])+"\n")
-            print "-- chbpm to",bpms[beats.index(onbeat)],"at beat",xyz,"--",
+            stepfile.writelines("chbpm "+repr(bpms[bpmchange_beats.index(onbeat)])+"\n")
+            print "-- chbpm to",bpms[bpmchange_beats.index(onbeat)],"at beat",xyz,"--",
+        for xyz in delay_beats:    #check to see if we need to wait some time
+          if xyz == onbeat:
+            stepfile.writelines("waits "+repr(delay_times[delay_beats.index(onbeat)]/1000)+"\n")
+            print "-- dalay of",delay_times[delay_beats.index(onbeat)]/1000,"sec. at beat",xyz,"--",
 
         dc = fileline[i]
+        dc1 = dc2 = ''       #initialized here so that they are declared even on EOF/EOL
         try:
           dc1 = fileline[i+1]
           dc2 = fileline[i+2]
-        except:
+        except:              #EOF or EOL
           neareof = 1
           
         i += 1
@@ -250,7 +249,7 @@ def main():
           fileline = msdfile.readline()
           i = 0
 
-        if dc1 == '!':    #freeze arrow
+        if (not neareof) and (dc1 == '!'):    #freeze arrow
           i += 2
           print 'hold',dc2,
 
@@ -286,14 +285,6 @@ def main():
         if steps:
 #          print stepline
           stepfile.writelines(stepline+"\n")
-
-    if skipnum == '' and bpm != '' and gap != '':
-      skipnum = int(gap / (60000/float(bpm/2))) - 1
-      print "because of gap setting, will skip",skipnum,"eighth notes."
-      offset = int(gap % (60000/bpm)) - 1
-      print "offset:  ",offset," (from gap of",gap,")"
-      stepfile.writelines("bpm "+"%.2f"%bpm+"\n")
-      stepfile.writelines("offset "+repr(offset)+"\n")
 
     fileline = msdfile.readline()
 
