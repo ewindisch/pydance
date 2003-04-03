9000e0d644b3ac0675b4a6c132daa76cabd4d9b4
tgz <tgz@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2003-04-03 20:19:47 +0000

    refactor from Jerub

--- utils/dwi2step.py
+++ utils/dwi2step.py
@@ -1,82 +1,197 @@
 #! /usr/bin/env python
 
 # dwi2step - converts DWI files from other clones to STEP files.
-# this could peobably be done way better than I'm doing it, but..
+
+# 2003-03-30 Refactoring done by Stephen Thorne <stephen@thorne.id.au>
 
 import os, pygame, pygame.font, pygame.image, pygame.mixer, pygame.movie, time, sys
 from pygame.locals import *
 
-VERSION = "0.51"
+VERSION = "0.52"
 
-# so it's currently in one routine. shut up, I'm learning python =]
+class DwiFile:
+  def __init__(self, filename):
+    self.filename = filename
+    self.file = open(filename)
+
+  def readline(self):
+    return self.file.readline()
+
+  def valid(self):
+    return self.file
+
+  def basename(self):
+    return self.filename[:-4]
+
+# Represents a single dance
+class Dance:
+  def __init__(self, category, difficulty):
+    self.category = category
+    self.difficulty = difficulty
+    self.moves = []
+
+  def addMove(self,move):
+    self.moves.append(move)
+
+  def addBeatChange(self, bc):
+    self.moves.append(bc)
+
+  def addPause(self, pause):
+    self.moves.append(Pause(pause))
+    
+  def getDance(self):
+    ret = "SINGLE\n%s %d\n" % (self.category, self.difficulty)
+    for move in self.moves:
+      ret += move.getLine()
+    ret += "end\n"
+    return ret
+
+# A detail about a dance, i.e. artist or song filename
+class Detail:
+  dwi2step = { 
+    '#ARTIST:':('group', 'groupname'), 
+    '#GAP:':('offset', 'gap'),
+    '#FILE:':('file', 'song filename'), 
+    '#TITLE:':('song', 'songname'),
+    '#BPM:':('bpm', 'bpm'),
+    '#GENRE:':('', 'genre')}
+
+  def __init__(self, label, detail):
+    self.detail = detail
+    (self.key, self.label) = self.dwi2step[label]
+
+    if self.key == 'file': self.detail = (self.detail[:-4]).lower() + ".mp3"
+    if self.key == 'offset': self.detail = - int(self.detail)
+    if self.key == 'bpm': self.detail = "%.2f" % float(self.detail)
+    
+  def getFound(self):
+    return "found %s: %s" % (self.label, self.detail)
+
+  def writeline(self, stepfile):
+    if self.key == '':
+      return
+    stepfile.writelines("%s %s\n" % (self.key, self.detail))
+
+# A beat
+class Beat:
+  stepmodes = {'{':('sixty',1/64.0), '[':('twtfr',1/24.0), '(':('steps',1.0), 
+    ')':('eight',2.0), ']':('eight',2.0), '}':('eight',2.0)}
+
+  def __init__(self, dc):
+    if dc == '': dc = ']'
+    (self.mode, self.time) = self.stepmodes[dc]
+
+  def getTime(self):
+    return self.time
+
+  def getMode(self):
+    return self.mode
+
+# Move, e.g. Up+Left
+class Move:
+  moves = {
+    '0':['0','0','0','0'], 
+    '1':['8','8','0','0'], 
+    '2':['0','8','0','0'], 
+    '3':['0','8','0','8'],
+    '4':['8','0','0','0'], 
+    '5':['0','0','0','0'], 
+    '6':['0','0','0','8'], 
+    '7':['8','0','8','0'],
+    '8':['0','0','8','0'], 
+    '9':['0','0','8','8'], 
+    'A':['0','8','8','0'], 
+    'B':['8','0','0','8']}
+
+  def __init__(self, dc, stepmode):
+    self.stepmode = stepmode
+    self.steps = self.moves[dc]
+    self.holds = self.moves['0']
+  
+  def hold(self, dc2):
+    self.holds = self.moves[dc2]
+
+  def getLine(self):
+    ret = self.stepmode.getMode()
+    for xy in zip(self.holds, self.steps):
+      ret += " " + xy[0] + xy[1]
+    return ret + "\n"
+
+class BeatChange:
+  def __init__(self, bpm):
+    self.bpm = bpm
+
+  def getLine(self):
+    return "chbpm "+repr(self.bpm)+"\n"
+
+  def getBeat(self):
+    return self.bpm
+
+class Pause:
+  def __init__(self, pause):
+    self.pause = pause
+
+  def getLine(self):
+    return "waits "+repr(self.pause/1000)+"\n"
+
+  def getPause(self):
+    return delay_times[delay_beats.index(xyz)]/1000
+
 def main():
   print "dwi2step, by theGREENzebra (tgz@clickass.org)"
 
+  if not len(sys.argv) > 1:
+    print "You need to actually specify an DWI file to convert!"
+    fileline = ''
+    return
+
+  print sys.argv[0]
+
+  print "Opening DWI file:",sys.argv[1],".."
+  msdf = reduce((lambda x,y:x+" "+y), sys.argv[1:])
+  if msdf[-4:] != ".dwi": 
+    if msdf[-4:] != ".DWI": 
+      msdf += ".dwi"
+  msdfile = DwiFile(msdf)
+
+  if not msdfile.valid():
+    print 'fatal error reading .dwi file, exiting'
+    return
+
+  convert(msdfile)
+
+def convert(msdfile):
   # set up
   print "Initialising.."
 
-# ARROW STATUSES in arrstat
-#-:   0 = no status / not initialised / top arrow
-#0:   1 = shown
-#1:   2 = pulse
-#2:   4 = step
-#3:   8 = onbeat
-#4:  16 = offbeat
-#5:  32 = way offbeat
-#6:  64 =
-#7: 128 = freeze
-
   # If I knew how to check whether a variable was defined, I would do
   # that instead
-  bpm = ''
-  gap = ''
-  stepmodes = ['sixty','twtfr','steps','eight','qurtr','halfn','whole']
-  stepbeats = [ 1/64.0, 1/24.0,     1 ,     2 ,     4 ,     8 ,    16 ]
-  rar = 0
   bpms = []
   delay_times = []
   bpmchange_beats = []
   delay_beats = []
+  stepfilename = msdfile.basename()+".step"
+  print 'Opening STEP file:',stepfilename,'..'
+  stepfile = open(stepfilename,"w") #but for writing
+  stepfile.write("version " + VERSION + "\n")
+  print "debugging crap follows"
+  print "----------------------"
+  print "wrote header"
 
-  print sys.argv[0]
+  dance = None
 
-  if len(sys.argv) > 1:
-    print "Opening DWI file:",sys.argv[1],".."
-    msdf = sys.argv[1]
-    if msdf[-4:] != ".dwi": 
-      if msdf[-4:] != ".DWI": 
-        msdf += ".dwi"
-    msdfile = open(msdf)
-    fileline = 'crapcrap'
-
-    stepfilename = msdf
-    stepfilename = stepfilename[:-4]+".step"
-    print 'Opening STEP file:',stepfilename,'..'
-    stepfile = open(stepfilename,"w") #but for writing
-    stepfile.write("version " + VERSION + "\n")
-    print "debugging crap follows"
-    print "----------------------"
-    print "wrote header"
-  else:
-    print "You need to actually specify an DWI file to convert!"
-    fileline = ''
-
-  while fileline != '':
+  line = '\n'
+  while line != '':
+    line = msdfile.readline()
     print '*',
-    if fileline != '\n':
-      fileline = fileline.strip()
-      if fileline[0:6] == '#FILE:':
-        songfile = fileline[6:(len(fileline)-5)]
-        print "found song filename: ", songfile
-        stepfile.writelines("file "+songfile.lower()+".mp3\n")
-      if fileline[0:7] == '#TITLE:':
-        songname = fileline[7:(len(fileline)-1)]
-        print "found songname: ", songname
-        stepfile.writelines("song "+songname+"\n")
-      if fileline[0:5] == '#BPM:':
-        bpm = float(fileline[5:(len(fileline)-1)])
-        print "found bpm: ", bpm
-        stepfile.writelines("bpm "+"%.2f"%bpm+"\n")
+    if line != '\n':
+      fileline = line.strip()
+      for i in ('#ARTIST:', '#GAP:', '#FILE:', '#TITLE:', '#BPM:', '#GENRE:'):
+        if fileline[0:len(i)] == i:
+          detail = Detail(i, fileline[len(i):-1])
+          print detail.getFound()
+          detail.writeline(stepfile)
+          
       if fileline[0:11] == '#CHANGEBPM:':
         parsemode = 1
         beat_string = ''
@@ -123,186 +238,82 @@ def main():
         for i in delay_beats:
           print "  at beat",i,"freeze",delay_times[delay_beats.index(i)]
         print
-      if fileline[0:8] == '#ARTIST:':
-        groupname = fileline[8:(len(fileline)-1)]
-        print "found groupname: ", groupname
-        stepfile.writelines("group "+groupname+"\n")
-      if fileline[0:5] == '#GAP:':
-        gap = int(fileline[5:-1])
-        print "found gap: ", gap, "( => offset = ", -gap, ")"
-        stepfile.writelines("offset "+repr(-gap)+"\n")
-      if fileline[0:14] == '#SINGLE:BASIC:':
+      if fileline[0:8] == '#SINGLE:':
         onbeat = 0
         neareof = 0
         bpmidx = 0
         delayidx = 0
-        diff = int(fileline[14])
-        print "\ndifficulty SINGLE BASIC,",diff,"feet. reading steps.."
-        stepfile.writelines("SINGLE\n"+"BASIC "+repr(diff)+"\n")
-        fileline = fileline[16:] + "\n"
         dc = ''
-        stepmode = "eight"
-        rar = 1
-      if fileline[0:14] == '#SINGLE:TRICK:':
-        onbeat = 0
-        neareof = 0
-        bpmidx = 0
-        delayidx = 0
-        diff = int(fileline[14])
-        print "\ndifficulty SINGLE TRICK,",diff,"feet. reading steps.."
-        stepfile.writelines("SINGLE\n"+"TRICK "+repr(diff)+"\n")
-        fileline = fileline[16:] + "\n"
-        dc = ''
-        stepmode = "eight"
-        rar = 1
-      if fileline[0:15] == '#SINGLE:MANIAC:':
-        onbeat = 0
-        neareof = 0
-        bpmidx = 0
-        delayidx = 0
-        diff = int(fileline[15])
-        print "\ndifficulty SINGLE MANIAC,",diff,"feet. reading steps.."
-        stepfile.writelines("SINGLE\n"+"MANIAC "+repr(diff)+"\n")
-        fileline = fileline[17:] + "\n"
-        dc = ''
-        stepmode = "eight"
-        rar = 1
-      if fileline[0:16] == '#SINGLE:ANOTHER:':
-        onbeat = 0
-        neareof = 0
-        bpmidx = 0
-        delayidx = 0
-        diff = int(fileline[16])
-        print "\ndifficulty SINGLE TRICK,",diff,"feet. reading steps.."
-        stepfile.writelines("SINGLE\n"+"TRICK "+repr(diff)+"\n")
-        fileline = fileline[18:] + "\n"
-        dc = ''
-        stepmode = "eight"
-        rar = 1
-      
+        stepmode = Beat('')
+        for i in ('BASIC','TRICK','MANIAC','ANOTHER'):
+          if fileline[8:8+len(i)] == i:
+            try:
+              difficulty = int(fileline[9+len(i):11+len(i)])
+            except:
+              difficulty = int(fileline[9+len(i)])
+            fileline = fileline[10+len(i):] + "\n"
+
+            if i == 'ANOTHER': # this is a trick.
+              i = 'TRICK'
+
+            print "\ndifficulty SINGLE %s, %s feet. reading steps.." % (i, difficulty)
+            dance = Dance(i, difficulty)
+            break
+            
       i = 0
 
-      while rar:
-        steps = []
-#        print "b"+repr(onbeat),
-
-        for xyz in bpmchange_beats[bpmidx:]:    #check to see if we need to change the bpm
-          if xyz <= onbeat:
-            stepfile.writelines("chbpm "+repr(bpms[bpmchange_beats.index(xyz)])+"\n")
-            print "-- chbpm to",bpms[bpmchange_beats.index(xyz)],"at beat",onbeat,"--",
-            bpmidx += 1
-        for xyz in delay_beats[delayidx:]:    #check to see if we need to wait some time
-          if xyz <= onbeat:
-            stepfile.writelines("waits "+repr(delay_times[delay_beats.index(xyz)]/1000)+"\n")
-            print "-- delay of",delay_times[delay_beats.index(xyz)]/1000,"sec. at beat",onbeat,"--",
-            delayidx += 1
-
+      while not dance == None and not fileline == '':
         dc = fileline[i]
-        dc1 = dc2 = ''       #initialized here so that they are declared even on EOF/EOL
-        try:
-          dc1 = fileline[i+1]
-          dc2 = fileline[i+2]
-        except:              #EOF or EOL
-          neareof = 1
-          
-        i += 1
 
-        print dc,
-        if dc == "0":
-          steps = ['00','00','00','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "1":
-          steps = ['08','08','00','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "2":
-          steps = ['00','08','00','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "3":
-          steps = ['00','08','00','08']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "4":
-          steps = ['08','00','00','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "5":
-          steps = ['00','00','00','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "6":
-          steps = ['00','00','00','08']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "7":
-          steps = ['08','00','08','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "8":
-          steps = ['00','00','08','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "9": 
-          steps = ['00','00','08','08']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "A":
-          steps = ['00','08','08','00']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        elif dc == "B":
-          steps = ['08','00','00','08']
-          onbeat += stepbeats[stepmodes.index(stepmode)]
-        if dc == "(":
-          stepmode = "steps"
-        if dc == ")":
-          stepmode = "eight"
-        if dc == "[":
-          stepmode = "twtfr"
-        if dc == "]":
-          stepmode = "eight"
-        if dc == "{":
-          stepmode = "sixty"
-        if dc == "}":
-          stepmode = "eight"
-        if dc == ";":
-          stepfile.writelines("end\n")
-          rar = 0
         if dc == "\n":
+          # Go to the next line, but continue reading the file.
           fileline = msdfile.readline()
           i = 0
+          continue
+        if dc == ";":
+          # The song is over!
+          stepfile.write(dance.getDance())
+          dance = None
+          break
 
-        if (not neareof) and (dc1 == '!'):    #freeze arrow
+        move = None
+        i += 1
+
+        if dc in ('0','1','2','3','4','5','6','7','8','9','A','B'): # Its a Move!
+          move = Move(dc, stepmode)
+          onbeat += stepmode.getTime()
+        elif dc in ('(',')','[',']','{','}'): # Its a Beat Change!
+          stepmode = Beat(dc)
+
+        if move == None:
+          continue
+
+        # Freeze Arrow Lookahead
+        if len(fileline)>i+1 and fileline[i] == '!':    
+          dc2 = fileline[i+1]
           i += 2
           print 'hold',dc2,
+          move.hold(dc2)
 
-          if dc2 == "1":
-            steps[0] =  '8'+steps[0][1]
-            steps[1] =  '8'+steps[1][1]
-          elif dc2 == "2":
-            steps[1] =  '8'+steps[1][1]
-          elif dc2 == "3":
-            steps[1] =  '8'+steps[1][1]
-            steps[3] =  '8'+steps[3][1]
-          elif dc2 == "4":
-            steps[0] =  '8'+steps[0][1]
-          elif dc2 == "6":
-            steps[3] =  '8'+steps[3][1]
-          elif dc2 == "7":
-            steps[0] =  '8'+steps[0][1]
-            steps[2] =  '8'+steps[2][1]
-          elif dc2 == "8":
-            steps[2] =  '8'+steps[2][1]
-          elif dc2 == "9": 
-            steps[2] =  '8'+steps[2][1]
-            steps[3] =  '8'+steps[3][1]
-          elif dc2 == "A":
-            steps[1] =  '8'+steps[1][1]
-            steps[2] =  '8'+steps[2][1]
-          elif dc2 == "B":
-            steps[0] =  '8'+steps[0][1]
-            steps[3] =  '8'+steps[3][1]
+        dance.addMove(move) # Add the move (and possibly the hold) to the dance
 
-        stepline = stepmode+" "
-        for stepwrite in steps:    stepline += stepwrite + " "
-        if steps:
-#          print stepline
-          stepfile.writelines(stepline+"\n")
+        for xyz in bpmchange_beats[bpmidx:]:    #check to see if we need to change the bpm
+          if onbeat >= xyz:
+            beatChange = BeatChange(bpms[bpmchange_beats.index(xyz)])
+            dance.addBeatChange(beatChange)
+            print "-- chbpm to",beatChange.getBeat(),"at beat",onbeat,"--"
+            bpmidx += 1
+        for xyz in delay_beats[delayidx:]:    #check to see if we need to wait some time
+          if onbeat >= xyz:
+            pause = Pause(delay_times[delay_beats.index(xyz)])
+            dance.addPause(pause)
+            print "-- delay of",pause.getPause(),"sec. at beat",onbeat,"--"
+            delayidx += 1
 
-    fileline = msdfile.readline()
+  # In case someone didn't put a ; at the end of a dance in a .dwi file.
+  if not dance == None:
+    stepfile.write(dance.getDance())
 
   print "End."
-#end
 
 if __name__ == '__main__': main()
