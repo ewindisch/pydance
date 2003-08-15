550446c23aadc9e63082e2ff4b6bc3263dbc0e89
piman <piman@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2003-08-15 06:07:49 +0000

    New graphics for game selector. Escape from game select takes you back one step rather than to the menu.

--- gameselect.py
+++ gameselect.py
@@ -12,60 +12,97 @@ SELECTORS = {
   "Endless": Endless,
   }
 
+# We can probably autogenerate this list somehow.
+# Maybe should also be in games?
+MODES = {
+  ("4P", "SINGLE"): "SINGLE",
+  ("4P", "VERSUS"): "VERSUS",
+  ("4P", "COUPLE"): "COUPLE",
+  ("4P", "DOUBLE"): "DOUBLE",
+
+  ("5P", "SINGLE"): "5PANEL",
+  ("5P", "VERSUS"): "5VERSUS",
+  ("5P", "COUPLE"): "5COUPLE",
+  ("5P", "DOUBLE"): "5DOUBLE",
+
+  ("8P", "SINGLE"): "6PANEL",
+  ("8P", "VERSUS"): "6VERSUS",
+  ("8P", "COUPLE"): "6COUPLE",
+  ("8P", "DOUBLE"): "6DOUBLE",
+
+  ("6P", "SINGLE"): "8PANEL",
+  ("6P", "VERSUS"): "8VERSUS",
+  ("6P", "COUPLE"): "8COUPLE",
+  ("6P", "DOUBLE"): "8DOUBLE",
+  }
+
 # Descriptions to output on the screen for each mode.
 DESCRIPTIONS = {
   "4P": "The standard up, down, left, and right arrows.",
   "5P": "Diagonals and the center.",
   "6P": "4 panel with a twist.",
   "8P": "Dance around the whole pad.",
+
+  "SINGLE": "Play by yourself.",
+  "VERSUS": "Challenge an opponent to the same steps.",
+  "COUPLE": "Two people dance complementary steps.",
+  "DOUBLE": "Try dancing on both pads at once.",
+
   "Normal": "One song at a time, or set up a playlist.",
   "Endless": "Keep dancing until you fail."
+
   }
-
-for mode in games.SINGLE: DESCRIPTIONS[mode] = "Play by yourself."
-
-for mode in games.VERSUS:
-  DESCRIPTIONS[mode] = "Challenge an opponent to the same steps."
-
-for mode in games.COUPLE:
-  DESCRIPTIONS[mode] = "Two people dance complementary steps."
-
-for mode in games.DOUBLE:
-  DESCRIPTIONS[mode] = "Try dancing on both pads at once."
 
 class GameSelect(object):
   def __init__(self, songitems, screen):
     self.screen = screen
+
+    # Store previously selected images, to blit for each choice.
+    self.images = [None, None, None]
 
     # FIXME: This can be changed to something more attractive later.
     self.bg = pygame.image.load(os.path.join(image_path, "bg.png"))
 
     # FIXME: We really need to do pretty animations and stuff.
 
-    mode = self.select_mode()
-    if mode is None: return
-    submode = self.select_submode(mode)
-    if submode is None: return
-    selector = self.select_selector()
-    if selector is None: return
-    
-    SELECTORS[selector](songitems, screen, submode)
+    # Track our current state so Esc takes us back to the previous choice,
+    # and not the menu.
+    states = [self.select_mode, self.select_submode, self.select_selector]
+    values = [None, None, None]
+    state = 0
+
+    while state != -1: # Esc pressed from first choice, return to menu
+      val = states[state]()
+      values[state] = val
+
+      # FIXME: When esc is pressed, return to the previously selected
+      # choice, not the initial choice.
+      if val is None: state -= 1
+      else: state += 1
+
+      if state == len(states): # All choices done, start the game
+        SELECTORS[values[2]](songitems, screen,
+                             MODES.get((values[0], values[1])))
+
+        # After playing, reset to the first choice.
+        # FIXME: Is going to the menu a better idea? Do more people exit the
+        # SS to change options, or to play a different game type?
+
+        values = [None, None, None]
+        self.images = [None, None, None]
+        state = 0
 
   def select_mode(self):
-    return self.select_general(["4P", "5P", "6P", "8P"])
+    return self.select_general(0, ["4P", "5P", "6P", "8P"])
 
-  def select_submode(self, mode):
-    return self.select_general({
-      "4P": ["SINGLE", "VERSUS", "COUPLE", "DOUBLE"],
-      "5P": ["5PANEL", "5VERSUS", "5COUPLE", "5DOUBLE"],
-      "6P": ["6PANEL"],
-      "8P": ["8PANEL"],
-      }[mode])
+  def select_submode(self):
+    return self.select_general(1, ["SINGLE", "VERSUS", "COUPLE", "DOUBLE"])
 
   def select_selector(self):
-    return self.select_general(["Normal", "Endless"])
+    return self.select_general(2, ["Normal", "Endless"])
 
-  def select_general(self, choices):
+  # idx is the index in self.images to modify with this choice.
+  def select_general(self, idx, choices):
     clock = pygame.time.Clock()
     images = {}
 
@@ -88,14 +125,21 @@ class GameSelect(object):
       elif ev == E_START:
         return choices[index]
 
+      elif ev == E_FULLSCREEN:
+        mainconfig["fullscreen"] ^= 1
+        pygame.display.toggle_fullscreen()
+
       index %= len(choices)
+      self.images[idx] = images[choices[index]]
 
       # Update the screen
       self.screen.blit(self.bg, [0, 0])
 
-      r = images[choices[index]].get_rect()
-      r.center = [320, 240]
-      self.screen.blit(images[choices[index]], r)
+      for i in range(len(self.images)):
+        if self.images[i] is not None:
+          r = self.images[i].get_rect()
+          r.center = [int(640.0 / (len(self.images) + 1) * (i + 1)), 240]
+          self.screen.blit(self.images[i], r)
 
       text = FONTS[28].render(DESCRIPTIONS[choices[index]], 1, colors.BLACK)
       r = text.get_rect()
@@ -105,4 +149,5 @@ class GameSelect(object):
       pygame.display.update()
       clock.tick(10)
 
+    self.images[idx] = None
     return None
