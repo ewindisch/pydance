6ab154390aff58ef39833fc9f45296ffdeaaca48
piman <piman@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2003-08-24 21:20:42 +0000

    Just Say No to tuple abuse.

--- colors.py
+++ colors.py
@@ -1,18 +1,18 @@
 # Colors and functions for working with colors
 
 color = {
-  'red': (255, 0, 0),
-  'pink': (255, 127, 127),
-  'yellow': (255, 255, 0),
-  'green': (0, 255, 0),
-  'cyan':(0, 255, 255),
-  'blue': (0, 0, 255),
-  'purple': (255, 0, 255),
-  'orange': (255, 127, 0),
-  'aqua': (0, 255, 127),
-  'white': (255, 255, 255),
-  'black': (0, 0, 0),
-  'gray': (127, 127, 127)
+  'red': [255, 0, 0],
+  'pink': [255, 127, 127],
+  'yellow': [255, 255, 0],
+  'green': [0, 255, 0],
+  'cyan': [0, 255, 255],
+  'blue': [0, 0, 255],
+  'purple': [255, 0, 255],
+  'orange': [255, 127, 0],
+  'aqua': [0, 255, 127],
+  'white': [255, 255, 255],
+  'black': [0, 0, 0],
+  'gray': [127, 127, 127]
   }
 
 # We use these a lot, so save lookup time
