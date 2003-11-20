d4b20f68f1b302a2ae0445b90199ffddaae86f7f
piman <piman@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2003-11-20 06:05:25 +0000

    ListBox with sample song list.

--- newss.py
+++ newss.py
@@ -8,10 +8,10 @@ from constants import *
 # This usually gets alphaed before stuff gets put in it.
 def make_box(color = [111, 255, 148], size = [130, 40]):
   s = pygame.Surface([size[0] + 8, size[1] + 8], SRCALPHA, 32)
-  print s.get_flags()
   s.fill(color + [100])
   r = s.get_rect()
-  for c in [[255, 255, 255], [212, 217, 255], [255, 252, 255], [88, 104, 255]]:
+  for c in [[255, 255, 255, 170], [212, 217, 255, 170],
+            [255, 252, 255, 170], [88, 104, 255, 170]]:
     pygame.draw.rect(s, c, r, 1)
     r.width -= 2
     r.height -= 2
@@ -82,7 +82,7 @@ class ActiveIndicator(pygame.sprite.Sprite):
     self.rect.topleft = topleft
 
   def update(self, time):
-    self.image.set_alpha(int(255 * (0.3 + (math.sin(time / 720.0) ** 2 / 3.0))))
+    self.image.set_alpha(int(255 * (0.3 + (math.sin(time / 720.0)**2 / 3.0))))
 
 # Box to indicate the current difficulty level.
 class DifficultyBox(pygame.sprite.Sprite):
@@ -111,33 +111,87 @@ class DifficultyBox(pygame.sprite.Sprite):
     self.image.set_alpha(140)
 
 SS_HELP = [
-  "Up / Down change song selection",
-  "Left / Right change difficulty setting (Left: Easier, Right: Harder)",
-  "Confirm / O / Up Right opens a folder or starts a song.",
-  "Cancel / X / Escape closes a folder or backs up.",
-  "Select / Tab takes you to a random song.",
-  "Start / Enter switches between screens.",
-  "F11 toggles fullscreen. S changes the sort mode.",
+  "Up / Down changes song selection",
+  "Left / Right changes difficulty setting",
+  "Confirm / O / Up Right opens a folder or starts a song",
+  "Cancel / X / Escape closes a folder or backs up",
+  "Select / Tab takes you to a random song",
+  "Start / Enter switches between screens",
+  "F11 toggles fullscreen - S changes the sort mode.",
   "Enjoy pydance 0.9.0!",
   ]
 
+ITEMS = [
+  "A Song",
+  "Another Song",
+  "B99U",
+  "Max 3.1415926535",
+  "N.M.R sucks a lot",
+  "I need more items",
+  "BlarrrrrrrrrR!",
+  "Cthulhu Fhtagn",
+  "Almost done",
+  "19 Smarch 2038",
+  "This is not nearly",
+  "enough songs yet.",
+  "Lousy huge new song",
+  "selection screen.",
+  "I'll show you!",
+  "This is the last one",
+  "OR IS IT?!"
+  ]
+  
 
-#class ListBox(pygame.sprite.Sprite):
-#  def __init__(self, items, spacing, count, topleft):
-#    self.s
-    
+class ListBox(pygame.sprite.Sprite):
+  def __init__(self, items, font, color, spacing, count, width, topleft):
+    pygame.sprite.Sprite.__init__(self)
+    self._items = items
+    self._idx = self._oldidx = 0
+    self._font = font
+    self._h = spacing
+    self._count = count
+    self._w = width
+    self._color = color
+    self._topleft = topleft
+    self._render()
+
+  def set_index(self, idx):
+    self._oldidx = self._idx
+    self._idx = idx
+
+  def update(self, time):
+    if self._idx != self._oldidx:
+      self._oldidx = self._idx
+      self._render()
+
+  def _render(self):
+    self.image = pygame.Surface([self._w, self._h * self._count], SRCALPHA, 32)
+    self.image.fill([0, 0, 0, 0])
+    for i, y in zip(range(self._count),
+                    range(self._h / 2, self._h * self._count, self._h)):
+      idx = (self._idx + i) % len(self._items)
+      t = fontfx.shadow(self._items[idx], self._font, 1, self._color,
+                        [c / 4 for c in self._color])
+      r = t.get_rect()
+      r.centery = y
+      r.left = 5
+      self.image.blit(t, r)
+    self.rect = self.image.get_rect()
+    self.rect.topleft = self._topleft
 
 class MainWindow(object):
   def __init__(self, screen, numplayers):
     self._screen = screen
     self._bg = pygame.image.load(os.path.join(image_path, "newss-bg.png"))
     self._sprites = pygame.sprite.RenderUpdates()
-    ActiveIndicator([405, 233]).add(self._sprites)
     diff1 = DifficultyBox(0, 2)
     diff1.set("MANIAC", [0, 255, 0], 7, "AA")
     diff2 = DifficultyBox(1, 2)
     diff2.set("TRICK", [255, 0, 0], 4, "C")
-    self._sprites.add([diff1, diff2])
+    self._list = ListBox(ITEMS, pygame.font.Font(None, 28), [255, 255, 255],
+                         26, 16, 220, [410, 56])
+    self._sprites.add([diff1, diff2, self._list])
+    ActiveIndicator([405, 233]).add(self._sprites)
     self._sprites.add(HelpText(SS_HELP, [255, 255, 255], [0, 0, 0],
                                pygame.font.Font(None, 18), [206, 20]))
     self._screen.blit(self._bg, [0, 0])
