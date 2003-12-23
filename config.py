eff64b9a788363859d2dd4e29f4b42a6d8fded32
piman <piman@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2003-12-23 14:16:46 +0000

    Cleanup.

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 # A generic configuration file parser
 
-# FIXME: We should make this subclass dict.
-
 import os
 
 # master vs user:
@@ -20,42 +18,38 @@ class Config(object):
 
   # Update with a dict object, instead of a file.
   def update(self, data, master = False):
-    if master:
-      for k in data: self.master[k] = data[k]
-    else:
-      for k in data: self.user[k] = data[k]
+    if master: self.master.update(data)
+    else: self.user[k].update(data)
         
   def __getitem__(self, key):
-    if self.user.has_key(key): return self.user[key]
-    elif self.master.has_key(key): return self.master[key]
-    else: return None
+    if key in self.user: return self.user[key]
+    else: return self.master.get(key)
   
   def __setitem__(self, key, value, master = False):
     if master: self.master[key] = value
     else: self.user[key] = value
 
   def __delitem__(self, key):
-    if self.master.has_key(key): del(self.master[key])
-    if self.user.has_key(key): del(self.user[key])
+    if key in self.master: del(self.master[key])
+    if key in self.user: del(self.user[key])
 
   def get(self, key, value = None):
-    if self.user.has_key(key): return self.user[key]
-    elif self.master.has_key(key): return self.master[key]
-    else: return value
+    if key in self.user: return self.user[key]
+    else: return self.master.get(key, value)
 
   # Update the config data with a 'key value' filename.
   # If shouldExist is true, raise exceptions if the file doesn't exist.
   # Otherwise, we silently ignore it.
-  def load(self, filename, master = False, shouldExist = False):
-    d = None
+  def load(self, filename, master = False, should_exist = False):
+    d = self.user
     if master: d = self.master
-    else: d = self.user
 
-    if not os.path.isfile(filename) and not shouldExist: return
+    if not os.path.isfile(filename) and not should_exist: return
 
-    fi = open(filename, "r")
+    fi = file(filename, "r")
     for line in fi:
-      if line.isspace() or len(line) == 0 or line[0] == '#': pass # comment
+      line = line.strip()
+      if not line or line[0] == '#': pass # comment
       else:
         key = line[0:line.find(' ')]
         val = line[line.find(' ') + 1:].strip()
@@ -69,10 +63,10 @@ class Config(object):
 
   # Write the filename back out to disk.
   def write(self, filename):
-    fi = open(filename, "w")
+    fi = file(filename, "w")
     keys = self.user.keys()
     keys.sort()
     for key in keys:
-      if not self.master.has_key(key) or self.master[key] != self.user[key]:
+      if key not in self.master or self.master[key] != self.user[key]:
         fi.write("%s %s\n" % (key, self.user[key]))
     fi.close()
