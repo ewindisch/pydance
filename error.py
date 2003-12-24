e5a46cf29406d51f07224ee184642bcad1adcbec
piman <piman@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2003-12-24 22:35:57 +0000

    Error screen matches other UI screens.

--- error.py
+++ error.py
@@ -1,36 +1,37 @@
-# Put up a pretty error message for a variety of programs.
+# Put up a pretty error message.
 
-from constants import *
+from interface import *
+from constants import mainconfig
+import fontfx
 import ui
 
 # FIXME: We should word-wrap text so we don't have to pass in a list.
 
-class ErrorMessage(object):
+class ErrorMessage(InterfaceWindow):
   def __init__(self, screen, lines):
-    screen.fill((0, 0, 0))
-    bg = pygame.image.load(os.path.join(image_path, "bg.png"))
-    bg.set_alpha(128)
-    screen.blit(bg, (0, 0))
-    text = FONTS[60].render("ERROR!", 1, (0, 0, 0))
+    InterfaceWindow.__init__(self, screen, "error-bg.png")
+    text = fontfx.shadow("Error!", 60, [255, 255, 255], offset = 2)
     text_rect = text.get_rect()
-    text_rect.center = (320, 30)
+    text_rect.center = (320, 50)
     screen.blit(text, text_rect)
 
     for i in range(len(lines)):
-      text = FONTS[32].render(lines[i], 1, (244, 244, 244))
+      text = fontfx.shadow(lines[i], 32, [200, 200, 200])
       text_rect = text.get_rect()
-      text_rect.center = (320, 36 * (i + 3))
+      text_rect.center = [320, 36 * (i + 3)]
       screen.blit(text, text_rect)
 
-    text = FONTS[32].render("Press Enter/Start/Esc", 1, (160, 160, 160))
-    text.set_colorkey(text.get_at((0, 0)))
+    text = fontfx.shadow("Press Enter/Start/Escape", 32, [160, 160, 160])
     textpos = text.get_rect()
-    textpos.center = (320, 440)
+    textpos.center = [320, 440]
     screen.blit(text, textpos)
 
-    pygame.display.flip()
-    ev = (0, ui.QUIT)
-    while ev[1] != ui.PASS: ev = ui.ui.poll()
-    while ev[1] != ui.START and ev[1] != ui.QUIT:
-      pygame.time.wait(50)
-      ev = ui.ui.poll()
+    pygame.display.update()
+    ui.ui.empty()
+    pid, ev = ui.ui.poll()
+
+    while ev not in [ui.START, ui.CONFIRM, ui.QUIT]:
+      if ev == ui.FULLSCREEN:
+        pygame.display.toggle_fullscreen()
+        mainconfig["fullscreen"] ^= 1
+      pid, ev = ui.ui.wait()
