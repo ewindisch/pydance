26d656ba059354fc89f2fc8646e91a85b449eb87
piman <piman@cb08dd6d-0ed5-0310-8e32-f00185bfeca8>
2004-01-12 01:43:09 +0000

    Reorder stat display. Clip rotating/zooming grade image. Mark top of lifebar chart with a white line. Increase lifebar chart sampling from 1 second to 0.5 seconds.

--- gradescreen.py
+++ gradescreen.py
@@ -35,6 +35,7 @@ class GradeSprite(pygame.sprite.Sprite):
     self._end = pygame.time.get_ticks() + 3000
     fn = os.path.join(image_path, "rating-%s.png" % rating)
     self._image = pygame.image.load(fn).convert()
+    self._size = self._image.get_size()
     self._center = center
     self.rect = self._image.get_rect()
     self.rect.center = center
@@ -43,10 +44,13 @@ class GradeSprite(pygame.sprite.Sprite):
     if time < self._end:
       angle = (self._end - time) / 3.0
       zoom = (1 - (self._end - time) / 3000.0)
-      self.image = pygame.transform.rotozoom(self._image, angle, zoom)
-      self.image = self.image.convert()
+      img = pygame.transform.rotozoom(self._image, angle, zoom).convert()
     else:
-      self.image = self._image
+      img = self._image
+    self.image = pygame.Surface(self._size)
+    r = img.get_rect()
+    r.center = self.image.get_rect().center
+    self.image.blit(img, r)
     self.image.set_colorkey(self.image.get_at([0, 0]))
     self.rect = self.image.get_rect()
     self.rect.center = self._center
@@ -72,6 +76,7 @@ class GrooveGaugeSprite(pygame.sprite.Sprite):
       if plife > 0.5: c = colors.average(c1, c2, (plife - 0.5) / 0.5)
       else: c = colors.average(c2, c3, plife / 0.5)
       pygame.draw.line(self._image, c, [i, size[1] - 1], [i, h])
+      if plife > 0.999999: self._image.set_at([i, 0], [255, 255, 255])
 
   def update(self, time):
     if time < self._end:
@@ -188,13 +193,13 @@ class GradingScreen(InterfaceWindow):
       StatSprite([200, 78], "GREAT:", plr.stats["G"], s, 666),
       StatSprite([200, 112], "OKAY:", plr.stats["O"], s, 1000),
       StatSprite([200, 146], "BOO:", plr.stats["B"], s, 1333),
-      StatSprite([200, 180], "MISS:", plr.stats["M"], s, 1666),
-      StatSprite([400, 10], "Early:", plr.stats.early, s, 666),
-      StatSprite([400, 44], "Late:", plr.stats.late, s, 1000),
+      StatSprite([200, 180], "MISS:", plr.stats["M"], s, 1333),
+      StatSprite([400, 10], "Score:", int(plr.score.score), s, 666),
+      HoldStatSprite([400, 44], "Holds:", plr.stats.good_holds,
+                     plr.stats.hold_count, s, 1000),
       StatSprite([400, 78], "Max Combo:", plr.stats.maxcombo, s, 1333),
-      HoldStatSprite([400, 112], "Holds:", plr.stats.good_holds,
-                     plr.stats.hold_count, s, 1666),
-      StatSprite([400, 146], "Score:", int(plr.score.score), s, 2000),
+      StatSprite([400, 112], "Early:", plr.stats.early, s, 1666),
+      StatSprite([400, 146], "Late:", plr.stats.late, s, 2000),
       StatSprite([400, 180], "TOTAL:", plr.stats.arrow_count, s, 2333)
       ])
     self._sprites.add(GradeSprite([98, 183], plr.grade.grade(plr.failed)))
@@ -210,12 +215,12 @@ class GradingScreen(InterfaceWindow):
         StatSprite([15, 372], "OKAY:", plr.stats["O"], s, 1000),
         StatSprite([15, 406], "BOO:", plr.stats["B"], s, 1333),
         StatSprite([15, 440], "MISS:", plr.stats["M"], s, 1666),
-        StatSprite([215, 270], "Early:", plr.stats.early, s, 666),
-        StatSprite([215, 304], "Late:", plr.stats.late, s, 1000),
+        StatSprite([215, 270], "Score:", int(plr.score.score), s, 666),
+        HoldSprite([215, 304], "Holds:", plr.stats.good_holds,
+                   plr.stats.hold_count, s, 1000),
         StatSprite([215, 338], "Max Combo:", plr.stats.maxcombo, s, 1333),
-        HoldStatSprite([215, 372], "Holds:", plr.stats.good_holds,
-                       plr.stats.hold_count, s, 1666),
-        StatSprite([215, 406], "Score:", int(plr.score.score), s, 2000),
+        StatSprite([215, 372], "Early:", plr.stats.early, s, 1666),
+        StatSprite([215, 406], "Late:", plr.stats.late, s, 2000),
         StatSprite([215, 440], "TOTAL:", plr.stats.arrow_count, s, 2333),
         ])
       self._sprites.add(GradeSprite([541, 294], plr.grade.grade(plr.failed)))
